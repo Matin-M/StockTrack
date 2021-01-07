4904a77c3a912a85912ad5140af6d515d2712689
Matin Massoudi <matin@massoudi.net>
2021-01-07 12:29:40 -0800

    Constraints fixes and added global markets view

---
+++ ViewControllers/GlobalMarketsView.swift
@@ -0,0 +1,20 @@
+//
+//  GlobalMarketsView.swift
+//  StockTrack
+//
+//  Created by Matin Massoudi on 1/7/21.
+//
+
+import UIKit
+
+class GlobalMarketsView: UIViewController {
+
+    //Chart containers.
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
+}
